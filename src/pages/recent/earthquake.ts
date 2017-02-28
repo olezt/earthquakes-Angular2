72bfc790f3e83107a7c3e44103b7cd62cd1fe5fc
Pmentz <lfants666@gmail.com>
2017-02-28 09:39:35 +0200

    Update earthquake class

--- src/pages/recent/earthquake.ts
+++ src/pages/recent/earthquake.ts
@@ -1,20 +1,6 @@
-import { Component } from '@angular/core';
-import { Injectable } from '@angular/core';
-
 export class Earthquake{
-        
-    recent: Earthquake[];
-    
-    constructor (private recentService: RecentService) {}
-    
-    ngOnInit() { 
-        this.getRecent(); 
-    }
-
-    getRecent() {
-        this.recentService.getRecent()
-            .subscribe(
-                recent => this.recent = recent);
-  }
-    
+  lat: number;
+  lon: number;
+  mag: number;
+  date: Date;
 }
